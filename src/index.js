8d88d89a1fadd201bc23d67d54d98a0df2e6ecbe
Ming-der Wang <mingderwang@gmail.com>
2022-03-01 12:15:58 +0800

    add ts-jest for testing

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleFunction = void 0;
+function sampleFunction(x) {
+    return x + x;
+}
+exports.sampleFunction = sampleFunction;
